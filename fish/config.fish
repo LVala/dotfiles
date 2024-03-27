1f9d891dc991d6d516eea03743b49bd10deba5df
Łukasz Wala <lukaszpwala@gmail.com>
2024-03-27 19:54:44 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,10 @@ set -gx tang_paths ~/repos/
 
 if status is-interactive
     set fish_greeting
-    set -q TMUX || tang misc
+
+    # in TTY or already in tmux? don't run tang
+    string match -q '/dev/tty*' (tty)
+    or set -q TMUX || tang -e misc
 end
+
+zoxide init fish | source
