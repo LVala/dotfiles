050e4362ffcbd437562c795780c18a1cc1ffb348
Łukasz Wala <lukaszpwala@gmail.com>
2024-08-30 12:18:20 +0200

    temporary

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,12 @@ if status is-interactive
     set fish_greeting
 
     alias ll="ls -lAh --color=auto --group-directories-first"
+    test -n "$TMUX" && alias clear="clear && tmux clear-history"
 
     # in TTY or already in tmux? don't run tang
     string match -q '/dev/tty*' (tty)
     or set -q TMUX || tang misc
 end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
