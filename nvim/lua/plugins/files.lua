a5380c12336109a0044227d808aa6f399e701f59
Łukasz Wala <lukaszpwala@gmail.com>
2025-07-17 22:47:59 +0200

    Minor neovim config updates

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -1,19 +1,18 @@
 return {
   "echasnovski/mini.files",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local mini_files = require("mini.files")
+  opts = {
+    mappings = {
+      close = "<Esc>",
+      go_in = "L",
+      go_in_plus = "l",
+    },
+  },
+  keys = function()
+    local mf = require("mini.files")
 
-    mini_files.setup({
-      mappings = {
-        close = "<Esc>",
-        go_in = "L",
-        go_in_plus = "l",
-      },
-    })
-
-    vim.keymap.set("n", "<leader>t", function()
-      if not mini_files.close() then mini_files.open() end
-    end)
-  end
+    return {
+      { "<leader>t", function() return not mf.close() and mf.open() end}
+    }
+  end,
 }
