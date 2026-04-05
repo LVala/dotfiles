b49dc8a7e46bad94c9cd1b9dd1f4f13673dddd5c
Łukasz Wala <lukaszpwala@gmail.com>
2026-04-05 22:23:25 +0200

    (neovim) Remove some unnecessary keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,14 +43,7 @@ vim.opt.listchars = {tab = "» ", trail = "•", nbsp = "␣"}
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "Q", "<nop>")
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
